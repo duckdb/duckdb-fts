ab06b53c02ef182892ed60d1eb027d0101634b06
dtenwolde <dljtw98@gmail.com>
2026-07-02 13:47:50 +0200

    make easier to statically link using command line option

--- extension_config.cmake
+++ extension_config.cmake
@@ -1,9 +1,13 @@
 # This file is included by DuckDB's build system. It specifies which extension to load
+set(FTS_DONT_LINK DONT_LINK)
+if (LINK_FTS_STATICALLY)
+    set(FTS_DONT_LINK "")
+endif()
 
 # Extension from this repo
 duckdb_extension_load(fts
-    SOURCE_DIR ${CMAKE_CURRENT_LIST_DIR}
-    INCLUDE_DIR ${CMAKE_CURRENT_LIST_DIR}/extension/fts/include
-    ${LOAD_FTS_TESTS}
-    DONT_LINK
+        SOURCE_DIR ${CMAKE_CURRENT_LIST_DIR}
+        INCLUDE_DIR ${CMAKE_CURRENT_LIST_DIR}/src/include
+        ${LOAD_FTS_TESTS}
+        ${FTS_DONT_LINK}
 )
